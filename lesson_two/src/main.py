9a6b06edb0d351e593006ff6a30dd5a587a92610
Nikita <118450758+NikitosZ112@users.noreply.github.com>
2025-01-24 15:51:48 +0400

    second commit commit V2.1

--- lesson_two/src/main.py
+++ lesson_two/src/main.py
@@ -1,56 +1,54 @@
 
 def search_array(arr, x):
-    start, end = 0, len(arr) -1
+    start, end = 0, len(arr) - 1
 
-    try:
-        if not isinstance(arr, list):
-            raise TypeError('Введенный Вами первый элемент не является списком')
-        if not all(isinstance(i, (int, type(None))) for i in arr):
-            raise TypeError('Список должен содержать целые числа или тип None')
-        if not isinstance(x, (int, type(None))):
-            raise TypeError('Искомый элемент должен быть целым числом или None')
+    if not isinstance(arr, list):
+        raise TypeError('Введенный Вами первый элемент не является списком')
+    if not all(isinstance(i, (int, type(None))) for i in arr):
+        raise TypeError('Список должен содержать целые числа или тип None')
+    if not isinstance(x, (int, type(None))):
+        raise TypeError('Искомый элемент должен быть целым числом или None')
 
-        while start <= end:
-            mid = (start + end) // 2
+    while start <= end:
+        mid = (start + end) // 2
 
-            if arr[mid] is None:
-                start_index, end_index = mid - 1, mid + 1
+        if arr[mid] is None:
+            start_index, end_index = mid - 1, mid + 1
 
-                while True:
-                    if start_index < start and end_index > end:
-                        return -1
-                    elif start <= start_index and arr[start_index] is not None:
-                        mid = start_index
-                        break
-                    elif end >= end_index and arr[end_index] is not None:
-                        mid = end_index
-                        break
-                    start_index -= 1
-                    end_index += 1
+            while True:
+                if start_index < start and end_index > end:
+                    return -1
+                elif start <= start_index and arr[start_index] is not None:
+                    mid = start_index
+                    break
+                elif end >= end_index and arr[end_index] is not None:
+                    mid = end_index
+                    break
+                start_index -= 1
+                end_index += 1
 
-            if arr[mid] == x:
-                return mid
-            elif arr[mid] < x:
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] is not None and x is not None:
+            if arr[mid] < x:
                 start += 1
             else:
                 end -= 1
+        elif x is None:
+            return -1
 
-        return -1
-
-
-    except TypeError:
-        print("Ошибка! Неверный тип входных данных.")
-        return ''
-    except IndexError:
-        print('Ошибка! Индекс списка находится вне диапазона')
-    except NameError:
-        print('Ошибка! Искомая переменная не определена.')
+    return -1
 
 arr = [1, 2, None, None, 5, 6, 7, None, 10, 11]
 
 print(search_array(arr, 8))
 print(search_array(arr, 11))
 print(search_array(arr, None))
-print(search_array(arr, 'fd'))
-print(search_array(arr, 37))
-print(search_array(arr, 3.7))
+print(search_array(arr, 5))
+print(search_array(arr, 2))
+print(search_array("АБВ", 5))
+print(search_array([1, 2.5, "АБВ"], 5))
+print(search_array(arr, ''))
+
+
+
--- lesson_two/test/test_search.py
+++ lesson_two/test/test_search.py
@@ -1,57 +1,22 @@
 import pytest
-from lesson_two.src.main import *
+from lesson_two.src.main import search_array
 
 @pytest.mark.parametrize(
-    'arr, x'
+    'arr, x, expected',
     [
-        ([i for i in range(10)], 7),
-        ([i for i in range(10)][::-1], 7)
-
+        ([1, 2, None, None, 5, 6, 7, None, 10, 11], 5, 4),   # Элемент присутствует
+        ([1, 2, None, None, 5, 6, 7, None, 10, 11], 11, 9),  # Элемент присутствует
+        ([1, 2, None, None, 5, 6, 7, None, 10, 11], None, 2), # Элемент None присутствует
+        ([1, 2, None, None, 5, 6, 7, None, 10, 11], 8, -1),   # Элемент отсутствует
+        ([1, 2], 'fd', ''),                                   # Некорректный тип (строка)
+        ([1, 2], 3.7, ''),                                   # Некорректный тип (число с плавающей запятой)
+        ([], 1, -1),                                         # Пустой массив
+        ("not a list", 5, TypeError),                       # Некорректный тип первого аргумента
     ]
 )
-
-def test_search_array(arr, x):
-    assert search_array(arr, x)
-
-# def search_in_sparce_array(arr, x):
-#     start, end = 0, len(arr) -1
-#
-#     while start <= end:
-#         mid = start + end // 2
-#
-#         if arr[mid] is None:
-#             start_index, end_index = mid - 1, mid + 1
-#
-#             while True:
-#                 if start >= start_index and end >= end_index:
-#                     return -1
-#                 elif start <= start_index and arr[start_index] is not None:
-#                     mid = start_index
-#                     break
-#                 elif end >= end_index and arr[end_index] is not None:
-#                     mid = end_index
-#                     break
-#                 start_index -= 1
-#                 end_index += 1
-#
-#         if arr[mid] == x:
-#             return mid
-#         elif arr[mid] < x:
-#             start += 1
-#         else:
-#             end -= 1
-#
-#     return -1
-#
-# arr = [1, 2, None, None, 5, 6, 7, None, 10, 11]
-#
-# print(search_in_sparce_array(arr, 5))
-# print(search_in_sparce_array(arr, 1))
-# print(search_in_sparce_array(arr, 9))
-#
-# #Tests
-#
-# def test_search_in_sparse_array():
-#     arr = [1, 2, None, None, 5, 6, 7, None, 10, 11]
-#
-#     assert search_in_sparce_array([], 1) == -1
+def test_search(arr, x, expected):
+    if isinstance(expected, type) and expected is TypeError:
+        with pytest.raises(TypeError):
+            search_array(arr, x)
+    else:
+        assert search_array(arr, x) == expected
