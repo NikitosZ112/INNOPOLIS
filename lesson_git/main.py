71cec99aefffcc0c0210735f66d577b44e7a8bfc
NikitosZ112 <118450758+NikitosZ112@users.noreply.github.com>
2025-03-23 14:42:33 +0400

    lesson_git

---
+++ lesson_git/main.py
@@ -0,0 +1,143 @@
+#
+# PS C:\Users\nikit\OneDrive\Рабочий стол\Lesson_IN> git version
+# git version 2.49.0.windows.1
+# PS C:\Users\nikit\OneDrive\Рабочий стол\Lesson_IN> git config --list
+# diff.astextplain.textconv=astextplain
+# filter.lfs.clean=git-lfs clean -- %f
+# filter.lfs.smudge=git-lfs smudge -- %f
+# filter.lfs.process=git-lfs filter-process
+# filter.lfs.required=true
+# http.sslbackend=openssl
+# http.sslcainfo=C:/Program Files/Git/mingw64/etc/ssl/certs/ca-bundle.crt
+# core.autocrlf=true
+# core.fscache=true
+# core.symlinks=false
+# pull.rebase=false
+# credential.helper=manager
+# credential.https://dev.azure.com.usehttppath=true
+# init.defaultbranch=master
+# filter.lfs.process=git-lfs filter-process
+# filter.lfs.required=true
+# filter.lfs.clean=git-lfs clean -- %f
+# :...skipping...
+# diff.astextplain.textconv=astextplain
+# filter.lfs.clean=git-lfs clean -- %f
+# filter.lfs.smudge=git-lfs smudge -- %f
+# filter.lfs.process=git-lfs filter-process
+# filter.lfs.required=true
+# http.sslbackend=openssl
+# http.sslcainfo=C:/Program Files/Git/mingw64/etc/ssl/certs/ca-bundle.crt
+# core.autocrlf=true
+# core.fscache=true
+# core.symlinks=false
+# pull.rebase=false
+# credential.helper=manager
+# credential.https://dev.azure.com.usehttppath=true
+# init.defaultbranch=master
+# filter.lfs.process=git-lfs filter-process
+# filter.lfs.required=true
+# filter.lfs.clean=git-lfs clean -- %f
+# filter.lfs.smudge=git-lfs smudge -- %f
+# user.name=NikitosZ112
+# user.email=118450758+NikitosZ112@users.noreply.github.com
+# core.autocrlf=true
+# core.repositoryformatversion=0
+# core.filemode=false
+# core.bare=false
+# core.logallrefupdates=true
+# core.symlinks=false
+# core.ignorecase=true
+# remote.origin.url=https://github.com/NikitosZ112/INNOPOLIS.git
+# remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+# branch.master.remote=origin
+# branch.master.merge=refs/heads/master
+# :...skipping...
+# diff.astextplain.textconv=astextplain
+# filter.lfs.clean=git-lfs clean -- %f
+# filter.lfs.smudge=git-lfs smudge -- %f
+# filter.lfs.process=git-lfs filter-process
+# filter.lfs.required=true
+# http.sslbackend=openssl
+# http.sslcainfo=C:/Program Files/Git/mingw64/etc/ssl/certs/ca-bundle.crt
+# core.autocrlf=true
+# core.fscache=true
+# core.symlinks=false
+# pull.rebase=false
+# credential.helper=manager
+# credential.https://dev.azure.com.usehttppath=true
+# init.defaultbranch=master
+# filter.lfs.process=git-lfs filter-process
+# filter.lfs.required=true
+# filter.lfs.clean=git-lfs clean -- %f
+# filter.lfs.smudge=git-lfs smudge -- %f
+# user.name=NikitosZ112
+# user.email=118450758+NikitosZ112@users.noreply.github.com
+# core.autocrlf=true
+# core.repositoryformatversion=0
+# core.filemode=false
+# core.bare=false
+# core.logallrefupdates=true
+# core.symlinks=false
+# core.ignorecase=true
+# remote.origin.url=https://github.com/NikitosZ112/INNOPOLIS.git
+# remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+# branch.master.remote=origin
+# branch.master.merge=refs/heads/master
+# ~
+# ~
+# ~
+# ~
+# ~
+# ~
+# ~
+# ~
+# (END)
+# diff.astextplain.textconv=astextplain
+# filter.lfs.clean=git-lfs clean -- %f
+# filter.lfs.smudge=git-lfs smudge -- %f
+# filter.lfs.process=git-lfs filter-process
+# filter.lfs.required=true
+# http.sslbackend=openssl
+# http.sslcainfo=C:/Program Files/Git/mingw64/etc/ssl/certs/ca-bundle.crt
+# core.autocrlf=true
+# :
+# diff.astextplain.textconv=astextplain
+# filter.lfs.clean=git-lfs clean -- %f
+# filter.lfs.smudge=git-lfs smudge -- %f
+# filter.lfs.process=git-lfs filter-process
+# filter.lfs.required=true
+# http.sslbackend=openssl
+# http.sslcainfo=C:/Program Files/Git/mingw64/etc/ssl/certs/ca-bundle.crt
+# core.autocrlf=true
+# core.fscache=true
+# core.symlinks=false
+# pull.rebase=false
+# credential.helper=manager
+# credential.https://dev.azure.com.usehttppath=true
+# init.defaultbranch=master
+# filter.lfs.process=git-lfs filter-process
+# filter.lfs.required=true
+# filter.lfs.clean=git-lfs clean -- %f
+# filter.lfs.smudge=git-lfs smudge -- %f
+# user.name=NikitosZ112
+# user.email=118450758+NikitosZ112@users.noreply.github.com
+# core.autocrlf=true
+# core.repositoryformatversion=0
+# core.filemode=false
+# core.bare=false
+# core.logallrefupdates=true
+# core.symlinks=false
+# core.ignorecase=true
+# remote.origin.url=https://github.com/NikitosZ112/INNOPOLIS.git
+# remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+# branch.master.remote=origin
+# branch.master.merge=refs/heads/master
+# ~
+# ~
+# ~
+# ~
+# ~
+# ~
+# ~
+# ~
+# (END)
